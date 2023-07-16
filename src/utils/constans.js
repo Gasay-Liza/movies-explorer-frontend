76e5267e600689433011168a17a77374e3b25264
Gasay-Liza <gasayliz@yandex.ru>
2023-07-16 22:55:55 +0300

    Change main api url

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,5 +1,5 @@
 export const headerPaths = ["/", "/movies", "/saved-movies", "/profile"];
 export const footerPaths = ["/", "/movies", "/saved-movies"];
 export const MOVIES_API_URL = "https://api.nomoreparties.co";
-export const MAIN_API_URL = "http://localhost:3000";
+export const MAIN_API_URL = "https://api.gasay-movies-explorer.nomoredomains.rocks";
 export const NAME_REG_EXP = "^[A-Za-zА-Яа-яЁё\\-\\s]+$"
