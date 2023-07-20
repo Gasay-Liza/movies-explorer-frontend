339b408d9939e08ba34c65ab67055c30042cdb64
Gasay-Liza <gasayliz@yandex.ru>
2023-07-20 12:16:58 +0300

    Fix: Fixed redirect on page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./PageNotFound.css";
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
   return (
     <div className="page__wrapper">
       <section className="not-found">
         <h3 className="not-found__title">404</h3>
         <p className="not-found__text">Страница не найдена</p>
-        <Link className="page__link not-found__link not-found__link_type_to-main" to="/">
+        <Link
+          className="page__link not-found__link not-found__link_type_to-main"
+          onClick={() => navigate(-1)}
+        >
           Назад
         </Link>
       </section>
